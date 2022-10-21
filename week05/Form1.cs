f8e59279474149f30ccf76572cac033ef72155ee
dmisi <mihaly.domokos@stud.uni-corvinus.hu>
2022-10-21 12:43:37 +0200

    commit4

--- week05/Form1.cs
+++ week05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using week05.Entites;
 using week05.MnbServiceReference;
 
@@ -21,12 +22,12 @@ namespace week05
         public Form1()
         {
             InitializeComponent();
-            Load();
             dataGridView1.DataSource = Rates;
+            XMLRead();
 
         }
 
-        private static void Load()
+        public  void XMLRead()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -38,6 +39,32 @@ namespace week05
             };
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+
+            }
+
+
+
         }
+
+      
+        
     }
 }
