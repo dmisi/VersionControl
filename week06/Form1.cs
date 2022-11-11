2316fdfacd2c2031c543e4f56f2f37541e9609fc
dmisi <mihaly.domokos@stud.uni-corvinus.hu>
2022-11-11 11:18:29 +0100

    commit1

--- week06/Form1.cs
+++ week06/Form1.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory ballFactory;
+
+        public BallFactory Factory
+        {
+            get { return ballFactory; }
+            set { ballFactory = value; }
+        }
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left>maxPosition)
+                {
+                    maxPosition = ball.Left;
+                }
+            }
+            if (maxPosition>1000)
+            {
+                var oldestball = _balls[0];
+                mainPanel.Controls.Remove(oldestball);
+                _balls.Remove(oldestball);
+            }
+        }
+        
     }
 }
