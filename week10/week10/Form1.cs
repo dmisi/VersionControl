051fa9dfe30a23bce703ea019da8c0fdab5d9368
dmisi <mihaly.domokos@stud.uni-corvinus.hu>
2022-12-09 12:11:07 +0100

    commit2

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace week10
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            //gc.AddPlayer();
+            //gc.Start(true);
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
